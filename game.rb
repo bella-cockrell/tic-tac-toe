6396f3461459e5669d6c6f2ac28b1424f1320487
bella-cockrell <arabella.cockrell@gmail.com>
2020-09-23 15:43:05 +0100

    passed checking input test

--- game.rb
+++ game.rb
@@ -1,3 +1,17 @@
 class Game
-    
+    attr_accessor :welcome_message, :start_input
+
+    def initialize
+        @welcome_message = "Welcome to the game, type 'start' to begin."
+        @start_input = ""
+    end
+
+    def start_game
+        puts @welcome_message
+        @start_input = gets.chomp
+    end
+
 end
+
+# game = Game.new
+# game.start_game
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,20 @@
-require_relative "../game"
+require_relative '../game'
 
 describe Game do
     game = Game.new
     context "when player loads program" do
         it "initializes the game object" do
             expect(!!(game)).to eq(true)
+        end
+        it "outputs a prompt" do
+            expect(game.welcome_message).to eq("Welcome to the game, type 'start' to begin.")
+        end
+        it "takes a player's input and checks that its start" do
+            #Arrange
+            
+            #Act/Assert
+            game.start_game
+            expect(game.start_input).to eq("Start")
         end
     end
 end
