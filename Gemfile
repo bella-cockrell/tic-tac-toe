ecc99648cf9f13428534c31f21bb48aff74d7084
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-20 14:50:41 +0100

    Adding Gemfile

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source 'https://rubygems.org'
-gem 'aruba', '~> 1.0.3'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem 'rspec'
--- cucumber/features/is_it_friday.rb
+++
@@ -1,9 +0,0 @@
-module FridayStepHelper
-    def is_it_friday(day)
-        if day == 'Friday'
-            return "TGIF"
-        else
-            return "Nope"
-        end
-    end
-end
