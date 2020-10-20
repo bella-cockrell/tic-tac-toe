eddbe92f8546bedff923b2f1f1efdc818f2912ee
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-20 10:18:47 +0100

    Refactoring tests for AI

--- spec/unit_tests/ai_spec.rb
+++ spec/unit_tests/ai_spec.rb
@@ -1,137 +1,123 @@
 require_relative "../../lib/ai.rb"
 
-
-
 describe AI do
     context "when x is about to win in a row" do
-
-        board_state = {[["x", "x", " "], [" ", " ", " "], [" ", " ", " "]] => [0, 2],
-                       [[" ", " ", " "], ["x", "x", " "], [" ", " ", " "]] => [1, 2],
-                       [["x", "x", " "], [" ", "o", "o"], [" ", " ", " "]] => [1, 0]
+        child_board = {[["x", "x", " "], [" ", " ", " "], [" ", " ", " "]] => [0, 2],
+                       [["o", " ", " "], ["x", "x", " "], [" ", "o", " "]] => [1, 2],
+                       [["x", "x", " "], [" ", "o", "o"], [" ", "x", " "]] => [1, 0]
                 }
-
         it "adds an o to block" do
             #arrange
             ai_input = AI.new
             #act/assert
-            board_state.each do |board, output|
+            child_board.each do |board, output|
                expect(ai_input.ai_move(board)).to eq(output) 
             end
-            
         end
     end
 
     context "when the ai checks the value of a row move" do
-        it "assigns infinity if its a winning move" do
+        it "assigns one if its a winning move" do
             ai_input = AI.new
-            board_state = [["x", "x", " "], ["o", "o", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [1, 2])
-            expect(processsed_value).to eq(Float::INFINITY)
+            child_board = [["x", "x", " "], ["o", "o", "o"], [" ", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [1, 2])
+            expect(processsed_value).to eq(1)
         end
         it "assigns 1 if [0, 2] is a blocking move to the top right" do
             ai_input = AI.new
-            board_state = [["x", "x", " "], ["o", " ", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [0, 2])
-            expect(processsed_value).to eq(1)
+            child_board = [["x", "x", "o"], ["o", " ", " "], [" ", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [0, 2])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [1,2] is a blocking move to the middle right" do
             ai_input = AI.new
-            board_state = [[" ", " ", " "], ["x", "x", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [1, 2])
-            expect(processsed_value).to eq(1)
+            child_board = [[" ", " ", " "], ["x", "x", "o"], [" ", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [1, 2])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [1,1] is a blocking move in the middle" do
             ai_input = AI.new
-            board_state = [[" ", " ", " "], ["x", " ", "x"], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [1, 1])
-            expect(processsed_value).to eq(1)
+            child_board = [[" ", " ", " "], ["x", "o", "x"], [" ", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [1, 1])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [1,0] is a blocking move to the top left" do
             ai_input = AI.new
-            board_state = [[" ", " ", " "], [" ", "x", "x"], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [1, 0])
-            expect(processsed_value).to eq(1)
+            child_board = [[" ", " ", " "], ["o", "x", "x"], [" ", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [1, 0])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [2,0] is a blocking move to the bottom left" do
             ai_input = AI.new
-            board_state = [[" ", " ", " "], [" ", " ", " "], [" ", "x", "x"]]
-            processsed_value = ai_input.move_value_calculator(board_state, [2, 0])
-            expect(processsed_value).to eq(1)
+            child_board = [[" ", " ", " "], [" ", " ", " "], ["o", "x", "x"]]
+            processsed_value = ai_input.move_value_calculator(child_board, [2, 0])
+            expect(processsed_value).to eq(nil)
         end
     end
     context "when the ai checks the value of a column move" do
         it "assigns 1 if [2,0] is a blocking move to the bottom left" do
             ai_input = AI.new
-            board_state = [["x", " ", " "], ["x", " ", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [2, 0])
-            expect(processsed_value).to eq(1)
+            child_board = [["x", " ", " "], ["x", " ", " "], ["o", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [2, 0])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [1,1] is a blocking move to the middle" do
             ai_input = AI.new
-            board_state = [[" ", "x", " "], [" ", " ", " "], [" ", "x", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [1, 1])
-            expect(processsed_value).to eq(1)
+            child_board = [[" ", "x", " "], [" ", "o", " "], [" ", "x", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [1, 1])
+            expect(processsed_value).to eq(nil)
         end
     end
     context "when the ai checks the value of a diagonal move" do
         it "assigns 1 if [2, 2] is a blocking move to the bottom right" do
             ai_input = AI.new
-            board_state = [["x", " ", " "], [" ", "x", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [2, 2])
-            expect(processsed_value).to eq(1)
+            child_board = [["x", " ", " "], [" ", "x", " "], [" ", " ", "o"]]
+            processsed_value = ai_input.move_value_calculator(child_board, [2, 2])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [1,1] is a blocking move to the middle" do
             ai_input = AI.new
-            board_state = [["x", " ", " "], [" ", " ", " "], [" ", " ", "x"]]
-            processsed_value = ai_input.move_value_calculator(board_state, [1, 1])
-            expect(processsed_value).to eq(1)
+            child_board = [["x", " ", " "], [" ", "o", " "], [" ", " ", "x"]]
+            processsed_value = ai_input.move_value_calculator(child_board, [1, 1])
+            expect(processsed_value).to eq(nil)
         end
-        it "assigns Infinity [2,0] it ISNT a blocking move but still on a diag" do
+        it "assigns one [2,0] it ISNT a blocking move but still on a diag" do
             ai_input = AI.new
-            board_state = [[" ", "x", " "], [" ", " ", "x"], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [2, 0])
-            expect(processsed_value).to eq(0)
+            child_board = [[" ", "x", " "], [" ", " ", "x"], ["o", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [2, 0])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [2,0] is a blocking move to the bottom left" do
             ai_input = AI.new
-            board_state = [[" ", " ", "x"], [" ", "x", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [2, 0])
-            expect(processsed_value).to eq(1)
+            child_board = [[" ", " ", "x"], [" ", "x", " "], ["o", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [2, 0])
+            expect(processsed_value).to eq(nil)
         end
         it "assigns 1 if [1,1] is a blocking move to the bottom left" do
             ai_input = AI.new
-            board_state = [[" ", " ", "x"], [" ", " ", " "], ["x", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [1,1])
-            expect(processsed_value).to eq(1)
+            child_board = [[" ", " ", "x"], [" ", "o", " "], ["x", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [1,1])
+            expect(processsed_value).to eq(nil)
         end
     end
     context "when the AI checks the value of a winning move" do
-        it "returns Infinity on a row" do
+        it "returns one on a row" do
             ai_input = AI.new
-            board_state = [["o", "o", " "], [" ", " ", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [0,2])
-            expect(processsed_value).to eq(Float::INFINITY) 
+            child_board = [["o", "o", "o"], [" ", " ", " "], [" ", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [0,2])
+            expect(processsed_value).to eq(1) 
         end
-        it "returns Infinity on a backwards diag" do
+        it "returns one on a backwards diag" do
             ai_input = AI.new
-            board_state = [[" ", " ", "o"], [" ", "o", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [2,0])
-            expect(processsed_value).to eq(Float::INFINITY) 
+            child_board = [[" ", " ", "o"], [" ", "o", " "], ["o", " ", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [2,0])
+            expect(processsed_value).to eq(1) 
         end
-        it "returns Infinity on a column" do
+        it "returns one on a column" do
             ai_input = AI.new
-            board_state = [[" ", "o", " "], [" ", "o", " "], [" ", " ", " "]]
-            processsed_value = ai_input.move_value_calculator(board_state, [2,1])
-            expect(processsed_value).to eq(Float::INFINITY) 
+            child_board = [[" ", "o", " "], [" ", "o", " "], [" ", "o", " "]]
+            processsed_value = ai_input.move_value_calculator(child_board, [2,1])
+            expect(processsed_value).to eq(1) 
         end
     end
-    # context "first move: when the middle square is NOT free" do
-    #     it "adds an o in the corner" do
-    #         #arrange
-    #         ai_input = AI.new
-    #         board_state = [[" ", " ", " "], [" ", "x", " "], [" ", " ", " "]]
-    #         #act/assert
-    #         expect(ai_input.ai_move(board_state)).to eq([1, 1])
-    #     end
-    # end
 end
